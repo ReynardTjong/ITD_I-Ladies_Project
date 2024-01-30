06f01faae4900bc351544a497060e2453c21f769
Reynard Tjong <S10245153@connect.np.edu.sg>
2024-01-30 13:45:59 +0800

    Fixing things

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class UiManager : MonoBehaviour
 {
@@ -489,4 +490,9 @@ public class UiManager : MonoBehaviour
 #endif
     }
     #endregion
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
