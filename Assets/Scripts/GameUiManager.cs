508a4ee04fc5e568f656a8537f0256c35b9155e9
Reynard Tjong <S10245153@connect.np.edu.sg>
2024-02-04 02:24:08 +0800

    Stressseeeddd

--- Assets/Scripts/GameUiManager.cs
+++ Assets/Scripts/GameUiManager.cs
@@ -10,9 +10,17 @@ public class GameUiManager : MonoBehaviour
     public FirebaseManager firebaseManager;
 
     [Header("Game Menu")]
-    public GameObject gameMenu;
+    [SerializeField] private GameObject gameMenu;
+    [SerializeField] private GameObject gameMenuBtn;
+    [SerializeField] private GameObject gameMenuContents;
+    [SerializeField] private GameObject gameMenuContentsBG;
+    [SerializeField] private GameObject gameMenuBtns;
 
+    [Header("Inventory Menu")]
+    [SerializeField] private GameObject inventoryScreen;
 
+    [Header("Quit Menu")]
+    [SerializeField] private GameObject quitGame;
 
     private void Awake()
     {
@@ -31,5 +39,41 @@ public class GameUiManager : MonoBehaviour
     private void Start()
     {
         firebaseManager = FirebaseManager.instance;
+    }
+
+    public void BackToGameMenuScreen()
+    {
+        gameMenuBtn.SetActive(true);
+        gameMenuContents.SetActive(false);
+    }
+
+    public void ShowGameMenuContents()
+    {
+        gameMenuBtn.SetActive(false);
+        gameMenuContents.SetActive(true);
+    }
+
+    public void BackToGameMenuContents()
+    {
+        gameMenuContentsBG.SetActive(true);
+        gameMenuBtns.SetActive(true);
+        inventoryScreen.SetActive(false);
+        quitGame.SetActive(false);
+    }
+
+    public void ShowInventoryScreen()
+    {
+        gameMenuContentsBG.SetActive(false);
+        gameMenuBtns.SetActive(false);
+        inventoryScreen.SetActive(true);
+        quitGame.SetActive(false);
+    }
+
+    public void ShowQuitScreen()
+    {
+        gameMenuContentsBG.SetActive(true);
+        gameMenuBtns.SetActive(false);
+        inventoryScreen.SetActive(false);
+        quitGame.SetActive(true);
     }
 }
