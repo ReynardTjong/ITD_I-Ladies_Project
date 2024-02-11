e343fe91ac3812ac42ef6bfd4c1e2c6a9b207028
Reynard Tjong <S10245153@connect.np.edu.sg>
2024-02-11 17:00:05 +0800

    Chapter 1 Debugging

--- Assets/Scripts/CampaignChapter1Manager.cs
+++ Assets/Scripts/CampaignChapter1Manager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR;
 
 public class CampaignChapter1Manager : MonoBehaviour
 {
@@ -20,36 +21,23 @@ public class CampaignChapter1Manager : MonoBehaviour
     [SerializeField] private GameObject checkpointOne;
     [SerializeField] private GameObject checkpointTwo;
     [SerializeField] private GameObject checkpointThree;
+    [SerializeField] private GameObject checkpointFour;
     [SerializeField] private GameObject checkpointFinal;
 
     [Header("Chapter 1 Texts Checkpoints")]
     [SerializeField] private GameObject scytheCheckpoint;
     [SerializeField] private GameObject trowelCheckpoint;
     [SerializeField] private GameObject wateringCheckpoint;
-    [SerializeField] private GameObject finaleCheckpoint;
+    [SerializeField] private GameObject finalCheckpoint;
 
     [Header("Chapter 1 Growth Stages")]
     [SerializeField] private GameObject growthSprouts;
-    [SerializeField] private GameObject middleStage;
-    [SerializeField] private GameObject finalStage;
+    [SerializeField] private GameObject middleToFinalStage;
 
     [Header("Chapter 1 Completion Canvas")]
     [SerializeField] private GameObject chapter1CompletionCanvas;
-    
 
-    // Start is called before the first frame update
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-        else if (Instance != this)
-        {
-            Destroy(gameObject);
-        }
-    }
+    private bool finalAreaOccupied = false;
 
     public void ShowQuestGarden()
     {
@@ -62,6 +50,7 @@ public class CampaignChapter1Manager : MonoBehaviour
         questIntro.SetActive(false);
         questGarden.SetActive(false);
 
+        chapter1QuestWalkthrough1.SetActive(false);
         chapter1QuestWalkthrough2.SetActive(true);
 
         ShowCheckpointOneTrigger();
@@ -74,8 +63,6 @@ public class CampaignChapter1Manager : MonoBehaviour
 
         chapter1QuestWalkthrough2.SetActive(false);
         chapter1QuestWalkthrough3 .SetActive(true);
-
-        ShowCheckpointTwoTrigger();
     }
     
     public void ShowCheckpointOneTrigger()
@@ -83,6 +70,7 @@ public class CampaignChapter1Manager : MonoBehaviour
         checkpointOne.SetActive(true); 
         checkpointTwo.SetActive(false);
         checkpointThree.SetActive(false);
+        checkpointFour.SetActive(false);
         checkpointFinal.SetActive(false);
     }
 
@@ -91,6 +79,7 @@ public class CampaignChapter1Manager : MonoBehaviour
         checkpointOne.SetActive(false);
         checkpointTwo.SetActive(true);
         checkpointThree.SetActive(false);
+        checkpointFour.SetActive(false);
         checkpointFinal.SetActive(false);
     }
 
@@ -98,7 +87,17 @@ public class CampaignChapter1Manager : MonoBehaviour
     {
         checkpointOne.SetActive(false);
         checkpointTwo.SetActive(true);
+        checkpointThree.SetActive(true);
+        checkpointFour.SetActive(false);
+        checkpointFinal.SetActive(false);
+    }
+
+    public void ShowCheckpointFourTrigger()
+    {
+        checkpointOne.SetActive(false);
+        checkpointTwo.SetActive(false);
         checkpointThree.SetActive(false);
+        checkpointFour.SetActive(true);
         checkpointFinal.SetActive(false);
     }
 
@@ -107,23 +106,16 @@ public class CampaignChapter1Manager : MonoBehaviour
         checkpointOne.SetActive(false);
         checkpointTwo.SetActive(true);
         checkpointThree.SetActive(false);
+        checkpointFour.SetActive(false);
         checkpointFinal.SetActive(false);
-    }
-
-    public void ShowScytheText()
-    {
-        scytheCheckpoint.SetActive(true);
-        trowelCheckpoint.SetActive(false);
-        wateringCheckpoint.SetActive(false);
-        wateringCheckpoint.SetActive(false);
     }
 
     public void ShowTrowelText()
     {
         scytheCheckpoint.SetActive(false);
         trowelCheckpoint.SetActive(true);
-        wateringCheckpoint.SetActive(false);
-        wateringCheckpoint.SetActive(false);
+        wateringCheckpoint.SetActive(false); 
+        finalCheckpoint.SetActive(false);
     }
 
     public void ShowWateringCanText()
@@ -131,7 +123,7 @@ public class CampaignChapter1Manager : MonoBehaviour
         scytheCheckpoint.SetActive(false);
         trowelCheckpoint.SetActive(false);
         wateringCheckpoint.SetActive(true);
-        wateringCheckpoint.SetActive(false);
+        finalCheckpoint.SetActive(false);
     }
 
     public void ShowFinalText()
@@ -139,54 +131,111 @@ public class CampaignChapter1Manager : MonoBehaviour
         scytheCheckpoint.SetActive(false);
         trowelCheckpoint.SetActive(false);
         wateringCheckpoint.SetActive(false);
-        finaleCheckpoint.SetActive(true);
+        finalCheckpoint.SetActive(true);
+    }
+
+    public void ShowGrowthSprouts()
+    {
+        growthSprouts.SetActive(true);
+        middleToFinalStage.SetActive(false);
+    }
+
+    public void ShowMiddleToFinalSprouts()
+    {
+        growthSprouts.SetActive(false);
+        middleToFinalStage.SetActive(true);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            ShowCheckpointOneTrigger();
+            ShowCheckpointTwoTrigger();
+            ShowQuestWalkthrough3();
         }
 
-        else if (other.CompareTag("Scythe"))
+        if (other.CompareTag("Scythe"))
         {
-            ShowCheckpointOneTrigger();
+            ShowCheckpointThreeTrigger();
+            ShowTrowelText();
         }
 
-        else if (other.CompareTag("Trowel"))
+        if (other.CompareTag("Trowel"))
         {
-            ShowCheckpointOneTrigger();
+            ShowCheckpointFourTrigger();
+            ShowWateringCanText();
+            ShowGrowthSprouts();
         }
 
-        else if (other.CompareTag("WateringCan"))
+        if (other.CompareTag("WateringCan"))
         {
-            ShowCheckpointOneTrigger();
+            ShowCheckpointFinalTrigger();
+            ShowFinalText();
+            ShowMiddleToFinalSprouts();
         }
 
-        else if (other.CompareTag("Bell Pepper"))
+        if (other.CompareTag("Bell Pepper"))
         {
-            ShowCheckpointOneTrigger();
+            CompleteChapter1(transform.GetSiblingIndex() + 1, true);
         }
     }
 
-    /*private void SpawnGoodJobUI()
+    public bool FinalAreaOccupied()
     {
-        if (firstTaskCompletionCanvas != null)
+        return finalAreaOccupied;
+    }
+
+    public void CompleteChapter1(int areaNumber, bool occupied)
+    {
+        if (areaNumber == 1)
         {
-            firstTaskCompletionCanvas.SetActive(true);
+            finalAreaOccupied = occupied;
+        }
+
+        if (FinalAreaOccupied())
+        {
+            SpawnGoodJobUI();
+        }
+    }
+
+    private void SpawnGoodJobUI()
+    {
+        if (chapter1CompletionCanvas != null)
+        {
+            chapter1CompletionCanvas.SetActive(true);
             StartCoroutine(CompleteFirstTaskCoroutine());
         }
         else
         {
             Debug.LogError("It's not set!");
         }
-    }*/
+    }
 
-    /* private IEnumerator CompleteFirstTaskCoroutine()
+     private IEnumerator CompleteFirstTaskCoroutine()
      {
          yield return new WaitForSeconds(3f);
 
          SceneManager.LoadScene("MainMenu");
-     }*/
+     }
+
+    //S Method to deactivate all Chapter 1 elements except the completion canvas
+    public void DeactivateChapter1SElements()
+    {
+        chapter1QuestWalkthrough1.SetActive(false);
+        chapter1QuestWalkthrough2.SetActive(false);
+        chapter1QuestWalkthrough3.SetActive(false);
+        questIntro.SetActive(false);
+        questGarden.SetActive(false);
+        checkpointOne.SetActive(false);
+        checkpointTwo.SetActive(false);
+        checkpointThree.SetActive(false);
+        checkpointFour.SetActive(false);
+        checkpointFinal.SetActive(false);
+        scytheCheckpoint.SetActive(false);
+        trowelCheckpoint.SetActive(false);
+        wateringCheckpoint.SetActive(false);
+        finalCheckpoint.SetActive(false);
+        growthSprouts.SetActive(false);
+        middleToFinalStage.SetActive(false);
+    }
 }
